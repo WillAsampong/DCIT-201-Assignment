074b8091206d5241428a97fabb76c139f90489c0
Will <asampongg377@gmail.com>
2025-01-27 21:18:44 +0000

    Question_3

--- DCIT-201/BankAccount.java
+++ DCIT-201/BankAccount.java
@@ -31,7 +31,7 @@ public class BankAccount {
     public void deposit(double amount) {
         if (amount > 0.0) {
             balance += amount;
-            System.out.println("Deposit successful. New balance: " + balance);
+            System.out.printf("Deposit successful. New balance: %.2f%n", balance);
         } else {
             throw new IllegalArgumentException("Deposit amount must be greater than 0.0");
         }
@@ -41,7 +41,7 @@ public class BankAccount {
         if(amount > 0.0){
             balance += amount;
             System.out.printf("Transaction note: %s%n", transactionalNote);
-            System.out.println("New balance: " + balance);
+            System.out.printf("New balance: %.2f%n", balance);
         } else {
             throw new IllegalArgumentException("Deposit amount must be greater than 0.0");
         }
@@ -54,27 +54,90 @@ public class BankAccount {
 
         if(amount <= balance) {
             balance -= amount;
-            System.out.println("Withdrawal successful. New balance: " + balance);
+            System.out.printf("Withdrawal successful. New balance: %.2f%n", balance);
         } else {
             System.out.println("Insufficient funds");
         }
     }
 
+    public void displayAccountDetails() {
+        System.out.println(accountHolderName);
+        System.out.println(accountNumber);
+        System.out.println(balance);
+    }
+
     public static void main(String[] args) {
-        try{
-            BankAccount accountholder1 = new BankAccount("Ukpel Derrick", "324360537237", 1000000.00);
+        BankAccount accountHolder1 = new savingsAccount("Asampong Godswill", "497634583453", 500.00, 3);
 
-            accountholder1.deposit(700.00);
-            accountholder1.deposit(850.00, "Salary Deposit");
+        BankAccount accountHolder2 = new currentAccount("Ukpel Derrick", "327469823647327", 1000.00, 500.00);
 
-            accountholder1.withdrawal(500.00);
+        accountHolder1.deposit(300.00);
+        accountHolder2.deposit(250.00);
 
+        accountHolder1.deposit(450.00, "Salary Deposit");
+        accountHolder2.deposit(700.00, "Payment for service");
 
+        accountHolder1.withdrawal(450.00);
+        accountHolder2.withdrawal(550.00);
+    }
+}
 
-        } catch(Exception e) {
-            System.err.println("Error message: " + e.getMessage());
+class savingsAccount extends BankAccount {
+    private double interestRate;
+    
+    public savingsAccount(String accountHolderName, String accountNumber, double balance, double interestRate) {
+        super(accountHolderName, accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double calculateInterest() {
+        return getBalance() * (interestRate / 100) * 12;
+    }
+
+    @Override
+    public void displayAccountDetails(){
+        super.displayAccountDetails();
+        System.out.println(interestRate);
+    }
+
+    @Override
+    public void withdrawal(double amount) {
+        if(getBalance() - amount >= 100) {
+            super.withdrawal(amount);
+        } else {
+            throw new IllegalArgumentException("Minimum balance requirement not met.");
         }
     }
 }
 
+class currentAccount extends BankAccount {
+    private double overdraftLimit;
 
+    public currentAccount(String accountHolderName, String accountNumber, double balance, double overdraftLimit){
+        super(accountHolderName, accountNumber, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public void displayAccountDetails(){
+        super.displayAccountDetails();
+        System.out.println(overdraftLimit);
+    }
+
+    @Override
+    public void withdrawal(double amount) {
+        if(super.getBalance() < overdraftLimit) {
+            super.withdrawal(amount);
+        } else {
+            throw new IllegalArgumentException("Overdraft limit exceeded.");
+        }
+    }
+}
